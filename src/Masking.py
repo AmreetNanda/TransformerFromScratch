6b6c46b3c2cd4f0d3435f963b48ba890d5ec959b
AmreetNanda <amreetnanda321@gmail.com>
2026-07-06 23:52:32 +0530

    Masking simple implementation added

---
+++ src/Masking.py
@@ -0,0 +1,15 @@
+import torch
+import numpy as np
+
+def single_head_mask(seq_len):
+    mask = torch.tril(torch.ones(seq_len, seq_len))
+    return mask.bool()
+
+def multi_head_mask(seq_len):
+    mask = torch.tril(torch.ones(seq_len, seq_len))
+    mask = mask.unsqueeze(0).unsqueeze(0)
+    return mask.bool()
+
+
+# mask = casual_mask(5)
+# print(mask)
